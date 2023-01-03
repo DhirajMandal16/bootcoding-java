8cd53bbacc06f27db9b664a5a87ebee9ded50e02
DhirajMandal16 <dhiraj.mandal.bca@ghrietn.raisoni.net>
2023-01-03 10:47:59 +0530

    Program to find even and odd number

---
+++ src/main/java/OddNumber.java
@@ -0,0 +1,10 @@
+public class OddNumber {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=2;i<=10;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
